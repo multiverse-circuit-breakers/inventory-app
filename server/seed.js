32fd42a51f0162a345618e2361d56a266b0fddce
Daniel Volchek <danielvolchek123@gmail.com>
2023-03-20 13:50:21 -0400

    modified seed to seed items instead of provided sauces

--- server/seed.js
+++ server/seed.js
@@ -1,21 +1,20 @@
-const {sauces, items} = require('./seedData.js');
+const { sauces, items } = require("./seedData.js");
 
-const {sequelize} = require('./db');
-const {Sauce} = require('./models');
+const { sequelize } = require("./db");
+const { Item } = require("./models");
 
 const seed = async () => {
+  try {
+    // drop and recreate tables per model definitions
+    await sequelize.sync({ force: true });
 
-    try {
-        // drop and recreate tables per model definitions
-        await sequelize.sync({ force: true });
-    
-        // insert data
-        await Promise.all(sauces.map(sauce => Sauce.create(sauce)));
+    // insert data
+    await Promise.all(items.map((item) => Item.create(item)));
 
-        console.log("db populated!");
-    } catch (error) {
-        console.error(error);
-    }
-}
+    console.log("db populated!");
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 seed();
