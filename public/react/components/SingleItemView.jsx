d30fc9e48e3fd125fde4417eb82a102aa704786f
Daniel Volchek <danielvolchek123@gmail.com>
2023-03-22 16:17:46 -0400

    feat: add first pass of single item view

--- public/react/components/SingleItemView.jsx
+++ public/react/components/SingleItemView.jsx
@@ -1,11 +1,28 @@
+import apiURL from "../api";
 import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 export const SingleItemView = () => {
+  const [item, setItem] = useState(null);
   const { id } = useParams();
 
-  return (
+  const fetchItem = async () => {
+    console.log("fetching item", id);
+    const res = await fetch(`${apiURL}/items/${parseInt(id) + 1}`);
+    const data = await res.json();
+    setItem(() => data);
+  };
+
+  // on render fetch the item from the db
+  useEffect(() => {
+    fetchItem();
+  }, []);
+
+  return item ? (
     <div>
-      <h1>This is the id: {id}</h1>
+      <h1>Title: {item.title}</h1>
     </div>
+  ) : (
+    <p>Loading...</p>
   );
 };
