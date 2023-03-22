8f1cc66ad501f437434e9bc20b63f0e3e19487dc
Daniel Volchek <danielvolchek123@gmail.com>
2023-03-22 13:58:03 -0400

    feat: concept version of react-router setup, next step is to implement with links to pages

--- public/react/components/App.js
+++ public/react/components/App.js
@@ -1,50 +1,72 @@
-import React, { useState, useEffect } from 'react';
-import { SaucesList } from './SaucesList';
-import { ItemsList } from './ItemsList'
-import { Item } from './Item'
+import React, { useState, useEffect } from "react";
+import { ItemsList } from "./ItemsList";
+import { Route, Routes, Link, useParams } from "react-router-dom";
 
 // import and prepend the api url to any fetch calls
-import apiURL from '../api';
+import apiURL from "../api";
+
+const ComponentWithID = () => {
+  const { id } = useParams();
+  return <div>ID is {id}</div>;
+};
 
 export const App = () => {
+  const [sauces, setSauces] = useState([]);
+  const [items, setItems] = useState([]);
 
-	const [sauces, setSauces] = useState([]);
-	const [items, setItems] = useState([])
+  async function fetchSauces() {
+    try {
+      const response = await fetch(`${apiURL}/sauces`);
+      const saucesData = await response.json();
+      setSauces(saucesData);
+    } catch (err) {
+      console.log("Oh no an error! ", err);
+    }
+  }
 
-	async function fetchSauces(){
-		try {
-			const response = await fetch(`${apiURL}/sauces`);
-			const saucesData = await response.json();
-			setSauces(saucesData); 
-		} catch (err) {
-			console.log("Oh no an error! ", err)
-		}
-	}
+  useEffect(() => {
+    fetchSauces();
+  }, []);
 
-	useEffect(() => {
-		fetchSauces();
-	}, []);
+  //GET fetch request for all items
+  async function fetchItems() {
+    try {
+      const res = await fetch(`${apiURL}/items`);
+      const itemsData = await res.json();
+      setItems(itemsData);
+    } catch (err) {
+      console.log("Error!", err);
+    }
+  }
 
-	//GET fetch request for all items
-	async function fetchItems(){
-		try {
-			const res = await fetch(`${apiURL}/items`);
-			const itemsData = await res.json();
-			setItems(itemsData)
-		} catch (err){
-			console.log('Error!', err)
-		}
-	}
+  useEffect(() => {
+    fetchItems();
+  }, []);
 
-	useEffect(() => {
-		fetchItems();
-	}, []);
-
-	return (
-		<main>	
-      		<h1 className='heading'>Sauce Store</h1>
-			<h3 id='heading2'>All things 🔥</h3>
-			<ItemsList items={items} />
-		</main>
-	)
-}
+  return (
+    <div>
+      <Link to="/">Home</Link>
+      <Link to="/items">items</Link>
+      {[1, 2, 3].map((id) => (
+        <Link to={`/item/${id}`}>Link to: {id}</Link>
+      ))}
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <main>
+              <h1>Home</h1>
+            </main>
+          }
+        />
+        <Route path="/item/:id" element={<ComponentWithID />} />
+      </Routes>
+    </div>
+  );
+};
+// temporary
+// {/* <main> */}
+// {/*   <h1 className="heading">Sauce Store</h1> */}
+// {/*   <h3 id="heading2">All things 🔥</h3> */}
+// {/*   <ItemsList items={items} /> */}
+// {/* </main> */}
