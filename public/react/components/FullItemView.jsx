c47192100f62058d3254b77b5fd5ebb0d01fb3d2
tanicha <tanijcha@gmail.com>
2023-03-23 12:50:23 -0700

    css feature: made main page components a little bigger, changed img size in edit item form, changed text color to white

--- public/react/components/FullItemView.jsx
+++ public/react/components/FullItemView.jsx
@@ -3,7 +3,7 @@ export const FullItemView = ({ item, actionButtons }) => {
     <div>
       <h1>Title: {item.title}</h1>
       <h3>Category: {item.category}</h3>
-      <img src={item.image} alt={item.title} />
+      <img className='edit-view-img'src={item.image} alt={item.title} />
       {actionButtons}
       <p>Price: ${item.price}</p>
       <p>{item.description}</p>
